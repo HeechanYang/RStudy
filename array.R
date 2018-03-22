6006c57dfb2b7653853baca016e5a0121c13a7bc
Yang <yhc944@gmail.com>
2018-03-22 18:22:17 +0900

    [ADD] array

---
+++ array.R
@@ -0,0 +1,15 @@
+x = c(1,2,3)
+y <- c(5,6,7)
+z = c(1,2)
+x + y
+x * y
+
+# [Warning message] : In x + z : longer object length is not a multiple of shorter object length
+# x + z
+# y * z
+
+length(x)
+length(y)
+length(z)
+a = 3
+length(a)
